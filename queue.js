6d07f1853ed2b64ca92534a25b5acf2deb575315
Kutlay Özger <kutlayozger@gmail.com>
2019-05-10 13:59:59 +0300

    Added queue

---
+++ queue.js
@@ -0,0 +1,27 @@
+
+
+
+class Queue {
+	constructor () {
+		this.collection = [];
+	}
+	
+	enqueue(item) {
+		this.collection.push(item);
+	}
+	
+	dequeue() {
+		return this.collection.shift();
+	}
+	
+	size() {
+		return this.collection.length;
+	}
+	
+	isEmpty() {
+		return this.collection.length == 0;
+	}
+}
+
+
+module.exports=Stack;
